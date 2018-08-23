24bcf2408482b9e03919956c2d2ceaea3b5eb594
MaxFeskov <maxfeskov@yandex.ru>
2018-08-23 17:45:16 +0300

    Инициализация webGL контекста

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,15 @@
-console.log('done');
+main();
+
+function main() {
+  const canvas = document.getElementById('canvas');
+
+  const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+
+    return;
+  }
+
+  console.log('done');
+}
